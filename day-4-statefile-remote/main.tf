00b909ae964edf538817c860697594a9f9b5d688
shindeapurva1119 <shindeapurva066@gmail.com>
2025-04-18 18:45:37 +0530

    day-4-both file

---
+++ day-4-statefile-remote/main.tf
@@ -0,0 +1,14 @@
+resource "aws_vpc" "dev" {
+    cidr_block = "10.0.0.0/16"
+  
+}
+resource "aws_subnet" "prod" {
+    cidr_block = "10.0.0.0/24"
+    vpc_id = aws_vpc.dev.id
+  
+}
+resource "aws_subnet" "sub" {
+    cidr_block = "10.0.1.0/24"
+    vpc_id = aws_vpc.dev.id
+
+}
